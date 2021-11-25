7e7007d4300a7ac6a382316b7e54a0d8b10092ce
dedyoktavianus65 <dedy.oktavianus43@gmail.com>
2021-11-25 19:00:58 +0800

    first commit

--- app/src/main/java/com/mirzayogy/a211androidn/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/a211androidn/MainActivity.kt
@@ -7,6 +7,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
